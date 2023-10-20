e145dcdb63e7d646804b26e27fad361704e12def
lyh109 <71144692+lyh109@users.noreply.github.com>
2023-10-21 01:20:18 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	int i;
+
+	cin >> s >> i;
+	cout << s[i - 1];
+
+	return 0;
+}
