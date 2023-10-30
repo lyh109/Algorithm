03c83836801f6bc9a122c667194ae676fd3aff60
lyh109 <71144692+lyh109@users.noreply.github.com>
2023-10-30 23:14:20 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < 2 * N; ++j)
+		{
+			if (j <= i || j >= (2 * N - 1 - i))
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << ' ';
+			}
+		}
+		cout << '\n';
+	}
+
+	for (int i = N - 1; i > 0; --i)
+	{
+		for (int j = 0; j < 2 * N; ++j)
+		{
+			if (j < i || j > (2 * N - 1 - i))
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << ' ';
+			}
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
