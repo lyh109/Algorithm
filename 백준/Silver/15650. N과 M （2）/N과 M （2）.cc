2b0ed680465481853ff52b9264e01f6b6e72bb9b
lyh109 <71144692+lyh109@users.noreply.github.com>
2023-11-18 01:28:27 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+
+	vector<int> a;
+	for (int i = 0; i < n; ++i)  a.push_back(i < m ? 0 : 1);
+
+	do {
+		for (int i = 0; i < n; ++i)
+		{
+			if(a[i] == 0) cout << i + 1 << ' ';
+		}
+		cout << '\n';
+	} while(next_permutation(a.begin(), a.end()));
+
+	return 0;
+}
