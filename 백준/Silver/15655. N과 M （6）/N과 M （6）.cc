efcd9ac44b39b46e60a62341b7c6af00052ebe17
lyh109 <71144692+lyh109@users.noreply.github.com>
2023-11-19 22:49:22 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int num[10];
+int arr[10];
+bool isUsed[10];
+
+void func(int k)
+{
+	if (k == m)
+	{
+		for(int i = 0; i < m; ++i)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if(isUsed[i]) continue;
+		if (k > 0)
+		{
+			if(arr[k - 1] > num[i]) continue;
+		}
+
+		arr[k] = num[i];
+		isUsed[i] = true;
+		func(k + 1);
+		isUsed[i] = false;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	for(int i = 0; i < n; ++i)
+		cin >> num[i];
+	sort(num, num + n);
+
+	func(0);
+
+	return 0;
+}
